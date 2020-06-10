20bc0e2e766aead834adc94898e9025901334d48
Diego Almeida <dyhalmeida@gmail.com>
2020-06-10 15:04:31 -0300

    added real time with socket.io

--- src/server.js
+++ src/server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 const cors = require('cors');
 const { resolve } = require('path');
+const routes = require('./routes');
 
 // Conexão com MongoDB
 require('./database/index').connection();
 
-const routes = require('./routes');
+io.on("connection", socket => {
+
+  socket.on("connectionRoom", box => {
+    socket.join(box);
+  })
+
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(resolve(__dirname, '..', 'uploads')));
+app.use((request, response, next) => {
+  request.io = io;
+  next();
+})
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
